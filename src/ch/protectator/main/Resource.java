242f6b2ec563dba8869248a036788ade408e3ce9
Kewin Dousse <kewin.d@websud.ch>
2014-04-10 20:36:31 +0200

    Adding Resource Class

---
+++ src/ch/protectator/main/Resource.java
@@ -0,0 +1,39 @@
+package ch.protectator.main;
+
+/**
+ * Represents the different existing Resources
+ * 
+ * @author Kewin Dousse
+ */
+public enum Resource {
+	METAL("Metal", 1),
+	CRYSTAL("Cristal", 2),
+	DEUTERIUM("Deuterium", 3);
+
+	private String name;
+	private int value;
+	
+	/**
+	 * @param name	Full name
+	 * @param value	Proportionally relative value
+	 */
+	Resource(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+}
